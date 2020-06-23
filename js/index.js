1cfc16ea99f02917dfa6cbacd64c00166c637dea
arkan481 <arkanharyo@gmail.com>
2020-06-23 15:35:09 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function rdrHobby() {
+    window.location = "../hobby.html";
+}
+
+function rdrOrg() {
+    window.location = "../organization.html";
+}
+
+function rdrBio() {
+    window.location = "../index.html";
+}
